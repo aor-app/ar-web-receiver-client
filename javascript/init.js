30f4d64e9a980aa3e7e9dd14abc03c4521d50324
aor-app <47556048+aor-app@users.noreply.github.com>
2019-03-26 16:59:29 +0900

    reset SERVER URL

--- javascript/init.js
+++ javascript/init.js
@@ -1,4 +1,3 @@
 /*! init.js | 2019/01 AOR, LTD. | www.aorja.com/receivers/ar-web-api/ */
-// const SERVER_URL = `${location.hostname}:${location.port}`;
-const SERVER_URL = `192.168.0.20:3000`;
+const SERVER_URL = `${location.hostname}:${location.port}`;
 
